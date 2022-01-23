9e6263ce0986b3f5eb76e43837f0c7c9f64a4d51
OHyDrSr9auy3a7tVS8Aw <97814800+OHyDrSr9auy3a7tVS8Aw@users.noreply.github.com>
2022-01-23 01:24:40 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h = {}
-
-        for i, n in enumerate(nums):
-            complement = target - n
+        seen = dict()
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
             try:
-                j = h[complement]
-                if i != j:
-                    return [i, j]
+                comp_idx = seen[complement]
+                if comp_idx != i:
+                    return [comp_idx, i]
             except KeyError:
-                h[n] = i
+                seen[num] = i
