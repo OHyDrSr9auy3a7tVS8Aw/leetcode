e9db00084c31628d53a91815ad845327804451f1
OHyDrSr9auy3a7tVS8Aw <97814800+OHyDrSr9auy3a7tVS8Aw@users.noreply.github.com>
2022-02-10 08:27:15 +0000

    Sync LeetCode submission - Linked List Cycle II (python3)

--- problems/linked_list_cycle_ii/solution.py
+++ problems/linked_list_cycle_ii/solution.py
@@ -6,25 +6,29 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
         if head is None:
             return
-
-        intersection = None
-        fast = head
-        slow = head
-
+        
+        intersect = None        
+        slow = fast = head
+        
         while fast.next is not None and fast.next.next is not None:
-            fast = fast.next.next
             slow = slow.next
-            if fast == slow:
-                intersection = fast
+            fast = fast.next.next
+            
+            if slow == fast:
+                intersect = slow
                 break
-
-        if intersection is not None:
-            p = head
-            q = intersection
-            while p != q:
-                p = p.next
-                q = q.next
-
-            return p
+    
+        if intersect is None:
+            return
+        
+        p = head
+        q = intersect
+        
+        while p != q:
+            p = p.next
+            q = q.next
+            
+        return p
