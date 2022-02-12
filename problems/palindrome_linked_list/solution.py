91132415578340429220767bfb88f981d42eac1a
OHyDrSr9auy3a7tVS8Aw <97814800+OHyDrSr9auy3a7tVS8Aw@users.noreply.github.com>
2022-02-12 01:49:36 +0000

    Sync LeetCode submission - Palindrome Linked List (python3)

--- problems/palindrome_linked_list/solution.py
+++ problems/palindrome_linked_list/solution.py
@@ -4,17 +4,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def mid(self, head: ListNode) -> ListNode:
+        slow = fast = head
+ 
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            
+        return slow
+    
+    def reverse(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+        
+        while curr is not None:
+            old_next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = old_next
+            
+        return prev
+    
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
         if head is None:
             return True
         
-        contiguous = list()
+        mid = self.mid(head)
         
-        curr = head
+        mid = self.reverse(mid)
         
-        while curr is not None:
-            contiguous.append(curr.val)
-            curr = curr.next
-            
-        return contiguous == list(reversed(contiguous))
+        p = head
+        q = mid
+        
+        while p is not None and q is not None:
+            if p.val != q.val:
+                return False
+            p = p.next
+            q = q.next
+        
+        return True
         
